d9b4212d08a8133b867bd63feab20c8028bcfdef
Albongle <alejandro.bongioanni@gmail.com>
2021-05-15 13:57:18 -0300

    Clase Producto

--- app/controllers/productosApi.php
+++ app/controllers/productosApi.php
@@ -1,5 +1,43 @@
 <?php
+require_once "./models/prodcuto.php";
+require_once "./interfaces/IApiUsable.php";
+class ProductosApi extends Producto implements IApiUsable{
 
+    public function TraerTodos($request, $response, $args)
+    {
+        $listraProductos = Empleado::TraerTodoLosEmpleados();
+        $jsonProdcuto = json_encode(array("listaProductos" => $listraProductos));
+        $response->getBody()->write($jsonProdcuto);
+        return $response
+          ->withHeader('Content-Type', 'application/json');
+    }
+
+    public function CargarUno($request, $response, $args)
+    {
+        $ArrayDeParametros = $request->getParsedBody();
+        $producto =  new Producto();
+        $producto->SetDatos($ArrayDeParametros['tipo'],$ArrayDeParametros['descripcion'],$ArrayDeParametros['stock'],$ArrayDeParametros['precio']);
+        if($producto->InsertarEmpleado()>0)
+        {
+            $response->getBody()->write("se guardo el Producto, ". $producto->MostrarDatos());
+        }
+        else{
+            $response->getBody()->write("No se guardo nada");
+        }
+        return $response;
+    }
+
+    public function TraerUno($request, $response, $args)
+    {
+        $id=$args['id'];
+    	$elProdcuto=Producto::TraerUnProducto($id);
+        $jsonProdcuto = json_encode(array("Prdocuto" => $elProdcuto));
+        $response->getBody()->write($jsonProdcuto);
+    	return $response;
+
+    }
+
+}
 
 
 
--- app/models/producto.php
+++ app/models/producto.php
@@ -1,25 +1,39 @@
 <?php
+require_once "./models/prodcuto.php";
+require_once "./db/AccesoDatos.php";
 require_once "./interfaces/IMostrarObjeto.php";
 class Producto implements IMostrarObjeto{
 
     public $idProdcutos;
+    public $tipo;
     public $descripcion;
     public $stock;
     public $precio;
+    const TIPO = array("Comida","Bebida");
 
     public function __construct()
     {
         
     }
-    public function SetDatos($descripcion, $stock,$precio)
+    public function SetDatos($tipo,$descripcion, $stock,$precio)
     {
+        $this->SetTipo($tipo);
         $this->descripcion =  $descripcion;
         $this->stock =  $stock;
         $this->precio =  $precio;
     }
+    private function SetTipo($value)
+    {
+        if (isset($value) && is_string($value) && in_array($value, self::TIPO)) {
+            $this->funcion =  $value;
+        } else {
+            
+            throw new Exception("Funcion del producto no permitida, solo se permite: Comida o Bebida");
+        }
+    }
     public function GetObjeto()
     {
-        return array("idProductos"=>$this->idProdcutos, "descripcion"=>$this->descripcion, "stock"=>$this->stock,"precio"=>$this->precio);
+        return array("idProductos"=>$this->idProdcutos,"tipo"=>$this->tipo, "descripcion"=>$this->descripcion, "stock"=>$this->stock,"precio"=>$this->precio);
     }
 
     /**
@@ -38,6 +52,33 @@ class Producto implements IMostrarObjeto{
         }
         $returnAux.="\r\n";
         return $returnAux;
+    }
+
+    public static function TraerUnProducto($id)
+    {
+        $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso();
+        $consulta =$objetoAccesoDato->RetornarConsulta("select * from prodcutos where idEmpleados = :id");
+        $consulta->bindValue(':id', $id, PDO::PARAM_INT);
+        $consulta->execute();
+        return $consulta->fetchAll(PDO::FETCH_CLASS, 'Prodcuto');
+    }
+    public static function TraerTodoLosEmpleados()
+    {
+        $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso();
+        $consulta =$objetoAccesoDato->RetornarConsulta("select * from productos");
+        $consulta->execute();
+        return $consulta->fetchAll(PDO::FETCH_CLASS, 'Prodcuto');
+    }
+    public function InsertarEmpleado()
+    {
+        $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso();
+        $consulta =$objetoAccesoDato->RetornarConsulta("INSERT INTO productos (tipo,descripcion,stock,precio)VALUES(:tipo,:descripcion,:stock,:precio)");
+        $consulta->bindValue(':tipo', $this->tipo, PDO::PARAM_STR);
+        $consulta->bindValue(':descripcion', $this->descripcion, PDO::PARAM_STR);
+        $consulta->bindValue(':stock', $this->stock, PDO::PARAM_INT);
+        $consulta->bindValue(':precio', $this->precio, PDO::PARAM_INT);
+        $consulta->execute();
+        return $objetoAccesoDato->RetornarUltimoIdInsertado();
     }
 
 }
