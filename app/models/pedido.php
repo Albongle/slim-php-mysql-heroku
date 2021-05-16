af6a1e1bdd961eceb728b5d9eada6f0556a522e5
Albongle <alejandro.bongioanni@gmail.com>
2021-05-15 23:00:34 -0300

    Clase Pedidos

--- app/models/pedido.php
+++ app/models/pedido.php
@@ -22,7 +22,7 @@ class Pedido implements IMostrarObjeto
         $this->idProducto =  $idProducto;
         $this->idMesa = $idMesa;
         $this->cantidad = $cantidad;
-        $this->SetHoraInicio(date("H:m:s",timestamp:time()));
+        $this->SetHoraInicio(date("H:i:s",time()));
         $this->horaEstimada =  $horaEstimada;
         $this->SetEstado("Iniciado");
     }
@@ -91,7 +91,7 @@ class Pedido implements IMostrarObjeto
         $this->SetEstado($estado);
         $consulta =$objetoAccesoDato->RetornarConsulta("UPDATE pedidos SET estado = :estado, horaFin = :horaFin WHERE idPedidos = :id");
         $consulta->bindValue(':estado', $this->$estado, PDO::PARAM_STR);
-        $consulta->bindValue(':horaFin', date("H:m:s",timestamp:time()), PDO::PARAM_STR);
+        $consulta->bindValue(':horaFin', date("H:i:s",time()), PDO::PARAM_STR);
         $consulta->bindValue(':id', $this->idPedidos, PDO::PARAM_INT);
         $consulta->execute();
         return true;
